89f6637adacf126ca537152f043426e342130a9e
Austin Dulay <dulayaustin@gmail.com>
2024-06-01 21:59:18 +0800

    Features test, successful submission link form

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,37 @@
+class LinksController < ApplicationController
+  before_action :set_link, only: [:show, :edit, :update, :destroy]
+
+  def index; end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+
+    if @link.save
+      redirect_to link_path(@link)
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+    def set_link
+      @link = Link.find(params[:id])
+    end
+
+    def link_params
+      params.require(:link).permit(:title, :url)
+    end
+end
